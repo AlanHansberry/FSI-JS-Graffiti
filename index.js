a4bad5ffb43a49e04d5bfb35455234b30a298cb8
Alan Hansberry <alan.hansberry02@gmail.com>
2022-02-22 19:16:22 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+// console.log(header)
+header.style.textAlign = "left"
+// selects dogImages and creates border radius
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i <dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
+//selects dogNames, aligns text left
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+// console.log(dogNames)
+//selects footertext and changes it's color
+let footertext = document.querySelector('.footer')
+footertext.style.color = 'red'
+
+//select dogimages and flips them
+// for(let i = 0; i < dogImages.length; i++){
+//     dogImages[i].style.transform = '(90deg)'
+// }
+// console.log(dogImages)
